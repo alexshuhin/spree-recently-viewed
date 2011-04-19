3a9c3993a136b449f8cf2600b00ee9bd8a75be47
Alexander Shuhin <a.medium@gmail.com>
2011-04-19 18:25:37 +0400

    Fixed recently view display, while showing product which in recently product list

--- app/controllers/products_controller_decorator.rb
+++ app/controllers/products_controller_decorator.rb
@@ -4,11 +4,11 @@ ProductsController.class_eval do
   def recently_viewed
     id = @product.id
     rvp = session['recently_viewed_products']
-    rvp = rvp.nil? ? [] : rvp.split(', ')
-    rvp.delete(id)
-    rvp << id unless rvp.include?(id.to_s)
-    rvp_max_count = Spree::RecentlyViewed::Config[:recently_viewed_products_max_count] || 5
-    rvp.delete_at(0) if rvp.size > rvp_max_count.to_i
+    rvp = rvp.nil? ? [] : rvp.split(', ').map(&:to_i)
+    rvp.reject!{|el| el == id}
+    rvp << id
+    rvp_max_count = Spree::RecentlyViewed::Config[:recently_viewed_products_max_count].to_i
+    rvp.delete_at(0) if rvp.size > rvp_max_count + 1
     session['recently_viewed_products'] = rvp.join(', ')
   end
 
--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -1,9 +1,13 @@
 Spree::BaseHelper.class_eval do
+  def recently_viewed_count
+    Spree::RecentlyViewed::Config[:recently_viewed_products_max_count].to_i
+  end
+
   def get_recently_viewed_products_ids
     if session['recently_viewed_products'].nil?
       []
     else
-      session['recently_viewed_products'].split(', ')
+      session['recently_viewed_products'].split(', ').map(&:to_i).reject{|el| el == @product.id}.last(recently_viewed_count)
     end
   end
   def get_recently_viewed_products
